0595feb8f866d343befd2808e7bcc12385b5a5d0
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-01-31 16:07:06 +0100

    components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import "./App.css";
-import data from "./data.json";
+
+import Section from "./components/Section";
 
 function App() {
   return (
     <div className="App">
       <header>NETFLIX</header>
       <main>
-        <div>
-          {data.map((list) => {
-            return (
-              <div>
-                <h1>{list.category}</h1>
-                <div>
-                  {list.images.map((picture) => {
-                    console.log(picture);
-                    return (
-                      <div>
-                        <img src={picture} alt="images" />
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-            );
-          })}
-          ;
-        </div>
+        <Section />
       </main>
-
       <footer>PIED DE PAGE</footer>
     </div>
   );
